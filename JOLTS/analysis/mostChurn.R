728e705e18ea2b7cf1e9b69a1d2fc3303295c39f
Bo Cowgill <bo_cowgill@bear-b.(none)>
2012-03-17 01:20:55 -0700

    Further developing churn per industry

--- JOLTS/analysis/mostChurn.R
+++ JOLTS/analysis/mostChurn.R
@@ -1,3 +1,11 @@
 rm(list=ls())
-d=read.table("../data/jt.data.4.TotalSeparations",head=T,fill=T)
+source("http://bocowgill.com/code.R")
+d=read.csv("../data/jt.data.4.TotalSeparations",sep="\t")
+seriesIds=read.csv("../data/jt.series",sep="\t")
+d=merge(d,seriesIds,by="series_id",all.x=T,all.y=F)
+industries=read.csv("../data/jt.industry",sep="\t")
+d=merge(d,industries,by="industry_code",all.x=T,all.y=F)
+
+output=data.frame(by(d$value,d$industry_text,sum))
+output=output[order(output$sum,decreasing=T),]
 
